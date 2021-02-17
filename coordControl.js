61962257a7f6a5af8e124fde4c955e173203cffe
Victor Russo <victor.rodrigues.russo@gmail.com>
2021-02-17 13:48:41 -0300

    kMeans with only two clusters and initial choose naive done

--- coordControl.js
+++ coordControl.js
@@ -15,12 +15,16 @@ var applicarion_state = "inserting";
 
 var cluster_colors = ["#4D4DEC","#F98500"];
 
-var analising_color = "#CD21210";
+var analising_color = "#CD2121";
 
 var num_clusters=2;
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
+}
+
+function euclidianDistance(pointA,pointB){
+    return Math.hypot(pointA.x-pointB.x,pointA.y-pointB.y);
 }
 
 
@@ -50,29 +54,56 @@ function on_click_clear_button(){
         applicarion_state = "inserting";
         num_points = 0;
         points_coordinates.length = 0;
-        console.log(points_coordinates);
+        //console.log(points_coordinates);
         $(".circle").remove();
     }
 }
 
 async function on_click_cluster_button(){
-    if(applicarion_state != "clustering"){
+    if(applicarion_state != "clustering" && applicarion_state != "clustered"){
         applicarion_state = "clustering";
         let centroids = [];
-        let indexes = [];
+        let indexes_togo = [];
+        for(var i=0;i<num_points;i++){
+            indexes_togo[i] = 1;
+        }
         for(var i=0;i<num_clusters;i++){
             let index;
             do{
                 index = getRandomInt(points_coordinates.length);
-            }while(indexes.includes(index));
-            indexes[i] = index;
-            console.log(index);
+            }while(indexes_togo[index] == 0);
+            indexes_togo[index] = 0;
+            //console.log(index);
             let centroid_aux = {x: points_coordinates[index].x,y:points_coordinates[index].y};
             centroids[i] = centroid_aux;
             $("#point"+index).css("background-color",cluster_colors[i]);
         }
         await delay(1000);
-        
+        let n_points_left = num_points - num_clusters;
+        while(n_points_left>0){
+            let index;
+            let distances = [];
+            do{
+                index = getRandomInt(points_coordinates.length);
+            }while(indexes_togo[index] == 0);
+            indexes_togo[index] = 0;
+            $("#point"+index).css("background-color",analising_color);
+            //console.log(index);
+            await delay(1000);
+            for(var i = 0;i<num_clusters;i++){
+                distances[i] = euclidianDistance(points_coordinates[index],centroids[i]);
+            }
+            cluster_index = distances.indexOf(Math.min(...distances));
+            centroids[cluster_index].x = (centroids[cluster_index].x+points_coordinates[index].x)/2;
+            centroids[cluster_index].y = (centroids[cluster_index].y+points_coordinates[index].y)/2;
+            $("#point"+index).css("background-color",cluster_colors[cluster_index]);
+            await delay(1000);
+
+
+            n_points_left--;
+        }
+
+
 
         applicarion_state = "clustered";
         //console.log(centroids);
